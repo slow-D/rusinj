55c6b1962aa16ce33e7b34abfb2eea727d1ae18b
shot <shot131@gmail.com>
2019-03-28 13:05:48 +0300

    Add ability to show inline content in modal window

--- js/includes/modal.js
+++ js/includes/modal.js
@@ -1,29 +1,46 @@
 let modal = (function($) {
-    let $container, $content;
+    let $container, $content, $innerContainer;
+    let modifier;
+
     return $.extend({
         opened: false,
-        open(content) {
+        setModifier(localModifier) {
+            modifier = localModifier;
+        },
+        open(content, title = '') {
             $content = $(content);
+            if (modifier) {
+                $container.find('.modal').addClass(modifier);
+            }
             this.emit('beforeOpen', $content);
-            $container.html($content);
+            $innerContainer.html($content);
             app.scrollLock();
-            $container.velocity('fadeIn');
+            $container.stop(true, false).fadeIn(300);
             this.emit('open', $content);
             this.opened = true;
         },
         close() {
             if (this.opened) {
-                $container.velocity('fadeOut', {
-                    complete() {
-                        $container.empty();
-                        this.opened = false;
-                        app.scrollUnlock();
+                $container.stop(true, false).fadeOut(300, () => {
+                    $innerContainer.empty();
+                    this.opened = false;
+                    if (modifier) {
+                        $container.find('.modal').removeClass(modifier);
+                        modifier = '';
                     }
+                    app.scrollUnlock();
                 });
             }
         },
         init() {
-            $container = $('<div class="overlay overlay_modal" data-id="replace-on-submit"></div>');
+            $container = $('<div class="overlay overlay_modal">' +
+                '<div class="modal">' +
+                '<div class="modal__close"></div>' +
+                '<div class="modal__container" data-id="replace-on-submit"></div>' +
+                '</div>' +
+                '</div>');
+            $innerContainer = $container.find('.modal__container');
+
             $('body').append($container);
             $('body').on('click', '.modal', ev => {
                 ev.stopImmediatePropagation();
@@ -36,21 +53,28 @@ let modal = (function($) {
             $('body').on('click', '.modal__close', () => {
                 this.close();
             });
+            $('body').keydown((e) => {
+                if (e.keyCode === 27) {
+                    this.close();
+                }
+            });
         }
-    }, new Events())
+    }, new Events());
 })(jQuery);
 
 app.on('init', () => {
     modal.init();
+
     $('body').on('click', 'a[data-ajax]', e => {
         e.preventDefault();
         let $this = $(e.currentTarget);
+        modal.setModifier($this.data('modal-modifier'));
         loader.show();
         $.ajax({
             type: $this.attr('method'),
             dataType: 'html',
             url: $this.attr('href'),
-            data: $this.data('post') ? executeFunctionByName($this.data('post'), window) : {},
+            data: $this.data('post') ? $this.data('post') : {},
             success: resp => {
                 modal.open(resp);
             },
@@ -58,5 +82,15 @@ app.on('init', () => {
                 loader.hide();
             }
         });
+    });
+
+    $('body').on('click', '[data-modal-inline]', function(e) {
+        e.preventDefault();
+        let $content = $($(this).data('modal-inline'));
+        if (!$content.length) {
+            return false;
+        }
+        modal.setModifier($(this).data('modal-modifier'));
+        modal.open($content[0].outerHTML, $(this).data('title'));
     });
 });
